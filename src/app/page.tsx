12d44d3d95a701d8f1b6243ddcf3b95ad1dff444
DanishDarius <nassoutoded@gmail.com>
2026-09-20 17:39:38 +0200

    Ajoute l'etagere de guides sur la page d'accueil

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import { CatalogueScreen } from "@/components/catalogue-screen";
+import { getGuideSummaries } from "@/lib/guides";
+
 export default function Home() {
-  return <CatalogueScreen home />;
+  return <CatalogueScreen home guides={getGuideSummaries()} />;
 }
