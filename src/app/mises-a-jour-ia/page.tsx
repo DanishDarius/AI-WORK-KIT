2b8b84f5693eb1eb8a395ed7e9994086465c2631
DanishDarius <nassoutoded@gmail.com>
2026-09-20 18:12:28 +0200

    Ajoute l'etagere de guides sur la page Mise a jour IA

--- src/app/mises-a-jour-ia/page.tsx
+++ src/app/mises-a-jour-ia/page.tsx
@@ -1,6 +1,7 @@
 import { IaUpdatesScreen } from "@/components/ia-updates-screen";
 import { iaUpdates } from "@/lib/ia-updates";
+import { getGuideSummaries } from "@/lib/guides";
 
 export default function MisesAJourIa() {
-  return <IaUpdatesScreen items={iaUpdates} />;
+    return <IaUpdatesScreen items={iaUpdates} guides={getGuideSummaries()} />;
 }
